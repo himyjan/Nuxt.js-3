949e2073eac35b7be101c600054cf363e4fb9db7
himyjan <51815522+himyjan@users.noreply.github.com>
2022-09-29 14:00:49 +0800

    update exlint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/color-mode'],
   typescript: {
     strict: true,
     typeCheck: {
