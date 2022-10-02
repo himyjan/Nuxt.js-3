eeab15b477d485d5185742e0134aeee78c165a2f
himyjan <51815522+himyjan@users.noreply.github.com>
2022-10-03 07:48:13 +0800

    add package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/color-mode'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@vueuse/nuxt',
+  ],
   typescript: {
     strict: true,
     typeCheck: {
