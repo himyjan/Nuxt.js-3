867a5294c211432e75fac0f5519712015e76d375
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-03 10:28:34 +0800

    update package version

--- composables/piniaStore.ts
+++ composables/piniaStore.ts
@@ -1,17 +1,35 @@
 import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
 
-export const myStore = defineStore({
+// Composition Api Stores
+export const myStore = defineStore('my-store2', () => {
+  const isReady = ref(false);
+
+  const changeIsReady = computed(() => !isReady.value);
+
+  const changeIsReady2 = () => {
+    isReady.value = !isReady.value;
+  };
+
+  return {
+    isReady,
+  };
+});
+
+// Options Api Stores
+export const myStore2 = defineStore({
   id: 'my-store',
   state: () => {
     return {
       isReady: false,
     };
   },
-});
-
-// Composing Stores
-export const myStore2 = defineStore('my-store2', () => {
-  return {
-    isReady: false,
-  };
+  getters: {
+    changeIsReady: (state) => !state,
+  },
+  actions: {
+    changeIsReady2() {
+      this.isReady = !this.isReady;
+    },
+  },
 });
