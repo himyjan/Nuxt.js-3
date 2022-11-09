d781a0e07485dacd23864df3b8d158b8595b9ca1
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-09 12:11:47 +0800

    upload code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,18 @@
 import { Config } from 'tailwindcss';
 
 export default <Config>{
-  content: ['./app.{vue}', './pages/**/*.{vue}', './components/**/*.{vue}'],
+  content: [
+    './app.{js,ts,vue}',
+    './App.{js,ts,vue}',
+    './error.{js,ts,vue}',
+    './Error.{js,ts,vue}',
+    './components/**/*.{js,vue,ts}',
+    './composables/**/*.{js,ts}',
+    './layouts/**/*.{vue}',
+    './pages/**/*.{vue}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   theme: {
     extend: {},
   },
