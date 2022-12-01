5f74bfb9ab79cc8afd67ce54ff4f8be41eab522c
himyjan <51815522+himyjan@users.noreply.github.com>
2022-12-01 11:39:33 +0800

    format code

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,7 +1,9 @@
-<script setup>
+<script setup lang="ts">
 const route = useRoute();
 </script>
 
 <template>
-  <div class="text-orange-500 text-[50px]">dynamic url: {{ route.path }}</div>
+  <div class="text-orange-500 text-[50px]">
+    dynamic url: {{ route.path }}
+  </div>
 </template>
